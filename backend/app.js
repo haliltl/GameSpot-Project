7d6b729ffcb05db79acc53d4dfeae680a1e0f450
bruhetan <ethan@etan.one>
2024-04-03 17:50:54 -0400

    added db connection

--- backend/app.js
+++ backend/app.js
@@ -3,9 +3,9 @@ const mongoose = require('mongoose');
 
 const app = express();
 
-mongoose.connect('mongodb://project:projectpassword123@99.247.239.46:27017/project-db', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-}).then(r => {
-    console.log('Connected to MongoDB');
+mongoose.connect('mongodb://project:projectpassword123@99.247.239.46:27017/project-db');
+
+const db = mongoose.connection;
+db.once('open', () => {
+    console.log('Connected to MongoDB at ' + db.host + ':' + db.port);
 });
