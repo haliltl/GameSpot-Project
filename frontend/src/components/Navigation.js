768f6e146112bba6c0912df63086ac35a12fbe60
haliltl <haliltugrul94@gmail.com>
2024-04-16 16:42:04 -0400

    These didnt work the first ime

---
+++ frontend/src/components/Navigation.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { Link, useMatch, useResolvedPath } from "react-router-dom";
+
+
+export default function Navigation() {
+
+  return (
+    <>
+    
+      <nav className="nav" >
+        <Link to="/" id="title">Game Spotter</Link>
+        <ul>
+          <CustomLink to="/">Home</CustomLink>
+        </ul>
+ 
+      </nav>
+      
+     
+    </>
+    
+  );
+};
+
+function CustomLink({ to, children, ...props }) {
+  const resolvedPath = useResolvedPath(to)
+  const isActive = useMatch({ path: resolvedPath.pathname, end: true })
+
+  return (
+    <li className={isActive ? "active" : ""}>
+      <Link to={to} {...props}>
+        {children}
+      </Link>
+    </li>
+  )
+
+}
+
+
