73aa7397f73c113dc23cec8c982e587d5e63d47e
bruhetan <ethan@etan.one>
2024-04-17 13:53:07 -0400

    clean up app.js

--- backend/app.js
+++ backend/app.js
@@ -3,23 +3,17 @@ const mongoose = require('mongoose');
 const passport = require('passport');
 const session = require('express-session');
 const bodyParser = require('body-parser');
-const bcrypt = require('bcrypt');
 const cors = require('cors');
-const jwt = require('jsonwebtoken');
 require('./passport-config')(passport);
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(cors({origin: 'http://localhost:3001', exposedHeaders: 'Authorization'}));
 app.use(session({ secret: 'secret', resave: true, saveUninitialized: true }));
 app.use(passport.initialize());
 app.use(passport.session());
-
-app.use(cors({
-  origin: 'http://localhost:3001',
-  exposedHeaders: 'Authorization',
-}));
 
 app.use('/user', require('./routes/user'));
 app.use('/game', require('./routes/game'));
