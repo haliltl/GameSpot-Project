2d6f6c8dc60e78b9f29552fbcf88a82f7440cff9
bruhetan <ethan@etan.one>
2024-04-17 13:17:15 -0400

    fix logout method

--- frontend/src/components/Logout.js
+++ frontend/src/components/Logout.js
@@ -2,11 +2,7 @@ import {useEffect} from "react";
 
 const Logout = () => {
   useEffect(() => {
-    const logout = async () => {
-      const response = await fetch("http://localhost:3000/auth/logout", {
-        method: "POST"
-      });
-    }
+    localStorage.removeItem('token');
     window.location.href = "/";
   }, []);
 }
